311bc87b0b6d7dac92387f0812bf7444fa09d53f
Maxim Chernyatevich <vechnoe.info@gmail.com>
2014-01-24 00:03:02 +0000

    Change entry_points.

--- setup.py
+++ setup.py
@@ -1,22 +1,34 @@
 # -*- coding: utf-8 -*-
-from setuptools import setup
+import os
+from setuptools import setup, find_packages
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+
+
+def read(fname):
+    try:
+        return open(os.path.join(os.path.dirname(__file__), fname)).read()
+    except IOError:
+        return ''
 
 setup(
     name='holydate',
-    version='0.1',
+    version=__import__('holydate').__version__,
     author='Maxim Chernyatevich',
     author_email='vechnoe.info@gmail.com',
     packages=['holydate'],
     url='https://github.com/vechnoe/holydate/',
     license='GNU General Public License v3 or later',
-    description=('This package represents the ancient '
-                 'Orthodox calendar for oldbeliever.'),
+    description=read('DESCRIPTION'),
+    keywords="calendar easter menology",
     long_description=open('README.rst').read(),
-    zip_safe=False,
     include_package_data=True,
     entry_points='''\
     [console_scripts]
-    holydate = holydate:main
+    holydate = holydate.cli:main
     ''',
     package_data = {'': ['README.rst']},
     install_requires=['pytils'],
