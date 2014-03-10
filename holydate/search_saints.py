2e15c9c9d7a8b5bae9da858e9e296e04126fd346
Maxim Chernyatevich <vechnoe.info@gmail.com>
2014-03-10 11:27:36 +0000

    Add feasts in serach.

--- holydate/search_saints.py
+++ holydate/search_saints.py
@@ -2,34 +2,24 @@
 # coding: utf-8
 
 """
-Serach saints in menology.
+Serach saints and feasts in year menology.
 
 """
 
 import re
 import datetime
+import calendar
 import textwrap
 from menology import menology
-from holydate_func import ju_to_gr_in_search
+from holydate import AncientCalendar
+from holydate_func import ju_to_gr_in_search, gr_to_ju_in_search
+
 
 def search_saints(search_string, mode='text'):
-    """Search saints in menology.
+    """Search saints and feasts in year menology.
     :param search_string: search input string (e.g. saint name).
     :param mode: text or html.
     """
-
-    search_string = search_string.decode('utf8')
-    d = menology
-    out = []
-    year = datetime.date.today().year
-    pattern = re.compile(ur'{0}*'.format(search_string), re.I | re.U)
-
-    #Ищем в menology строку; если есть, добавляем в out.
-    for key, value in d.iteritems():
-        for key1, value1 in d[key].iteritems():
-            for key2, value2 in d[key][key1].iteritems():
-                if re.search(pattern, str(value2).decode('utf8')):
-                    out.extend([[ju_to_gr_in_search(key1, key, year), [key1, key], [value2]]])
 
     month_word = {
         1: 'января',
@@ -45,6 +35,33 @@ def search_saints(search_string, mode='text'):
         11: 'ноября',
         12: 'декабря'
     }
+
+    search_string = search_string.decode('utf8')
+    year = datetime.date.today().year
+    year_menology = menology
+    cal_out = []
+    
+    #List of weekday names.
+    for month in range(1, 13):
+        days_in_month = calendar.monthrange(year, month)[1]
+        for day in range(1, days_in_month + 1):
+            cal = AncientCalendar(day, month, year)
+            weekdayname = cal.getWeekdayname()
+            cal_out.extend([[gr_to_ju_in_search(day, month, year), weekdayname]])
+
+    for i in cal_out:
+        year_menology[i[0][1]][i[0][0]]['weekday'] = i[1]
+
+    d = year_menology
+    out = []
+    pattern = re.compile(ur'{0}*'.format(search_string), re.I | re.U)
+
+    #Ищем в menology строку; если есть, добавляем в out.
+    for key, value in d.iteritems():
+        for key1, value1 in d[key].iteritems():
+            for key2, value2 in d[key][key1].iteritems():
+                if re.search(pattern, str(value2).decode('utf8')):
+                    out.extend([[ju_to_gr_in_search(key1, key, year), [key1, key], [value2]]])
 
     #Меняем элементы вложенного списка на значения словаря с названиями месяцев.
     for item in out:
@@ -70,6 +87,7 @@ def search_saints(search_string, mode='text'):
                           str(item[1][0]) + ' ' + item[1][1] + ' по ст. ст.' + '</span>' + '<br>' + '\n' +\
                           '<span class="saint">' + str(item[2][0]).rstrip() + '\n' + '</span>' + '<br><br>' + '\n\n'
         return string_out
+
 
 if __name__ == "__main__":
 
